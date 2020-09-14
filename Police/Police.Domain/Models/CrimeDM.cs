91f5aafe0a2cc011a51390e75457eef8caa4a10b
Asher Williams <asher.williams1998@gmail.com>
2020-09-14 13:22:03 -0500

    created Crime domain model

---
+++ Police/Police.Domain/Models/CrimeDM.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Police.Domain.Models
+{
+    public class CrimeDM
+    {
+        private string _classification;
+
+        public int CrimeID { get; set; }
+        public string Classification 
+        {
+            get => _classification;
+            set
+            {
+                if (value.Length == 0)
+                {
+                    throw new ArgumentException("Classification cannot be empty", nameof(value));
+                }
+                _classification = value;
+            }
+        }
+
+        public CrimeDM(int ID, string classification)
+        {
+            CrimeID = ID;
+            Classification = classification;
+        }
+
+        public CrimeDM() { }
+    }
+}
