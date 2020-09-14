2ca8943886d243d57bbb6e66970470cc9bcdfbd6
Asher Williams <asher.williams1998@gmail.com>
2020-09-14 14:09:30 -0500

    created police report domain model

---
+++ Police/Police.Domain/Models/PoliceReportDM.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Police.Domain.Models
+{
+    public class PoliceReportDM
+    {
+        private string _id;
+        private string _narrative;
+
+        public string ID 
+        {
+            get => _id;
+            set
+            {
+                if (value.Length == 0)
+                {
+                    throw new ArgumentException("Police report ID cannot be empty", nameof(value));
+                }
+                _id = value;
+            }
+        }
+        public DateTime TimeOccurred { get; set; }
+        public DateTime TimeReported { get; set; }
+        public string Narrative 
+        {
+            get => _narrative; 
+            set
+            {
+                if (value.Length == 0)
+                {
+                    throw new ArgumentException("Narrative cannot be empty", nameof(value));
+                }
+                _narrative = value;
+            }
+        }
+        public int CrimeID { get; set; }
+        public int CrimeLocation { get; set; }
+
+        public PoliceReportDM(string id, DateTime timeOccurred, DateTime timeReported, string narrative, int crime, int address)
+        {
+            ID = id;
+            TimeOccurred = timeOccurred;
+            TimeReported = timeReported;
+            Narrative = narrative;
+            CrimeID = crime;
+            CrimeLocation = address;
+        }
+
+        public PoliceReportDM() { }
+    }
+}
