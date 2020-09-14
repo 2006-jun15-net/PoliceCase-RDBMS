e36877a7880966be576bbc178aaba6b0108b7c8a
Asher Williams <asher.williams1998@gmail.com>
2020-09-14 11:32:24 -0500

    added constructor and added checks on reportID and ArrestingOfficer

--- Police/Police.Domain/Models/ArrestingReportDM.cs
+++ Police/Police.Domain/Models/ArrestingReportDM.cs
@@ -6,10 +6,46 @@ namespace Police.Domain.Models
 {
     public class ArrestingReportDM
     {
-        public string ReportID { get; set; }
-        public string ArrestingOfficer { get; set; }
+        private string _reportID;
+        private string _arrestingOfficer;
+
+        public string ReportID 
+        {
+            get => _reportID;
+            set
+            {
+                if (value.Length == 0)
+                {
+                    throw new ArgumentException("Report ID cannot be empty", nameof(value));
+                }
+                _reportID = value;
+            }
+        }
+        public string ArrestingOfficer 
+        {
+            get => _arrestingOfficer; 
+            set
+            {
+                if (value.Length == 0)
+                {
+                    throw new ArgumentException("Badge number of arresting officer cannot be empty", nameof(value));
+                }
+                _arrestingOfficer = value;
+            }
+        }
         public DateTime ArrestDate { get; set; }
         public int CriminalID { get; set; }
         public int CrimeID { get; set; }
+
+        public ArrestingReportDM(string reportID, string officer, DateTime arrestDate, int criminal, int crime)
+        {
+            ReportID = reportID;
+            ArrestingOfficer = officer;
+            ArrestDate = arrestDate;
+            CriminalID = criminal;
+            CrimeID = crime;
+        }
+
+        public ArrestingReportDM() { }
     }
 }
