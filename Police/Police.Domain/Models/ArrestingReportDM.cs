33be431033cd58e13f583f93f447fac9651fae05
Asher Williams <asher.williams1998@gmail.com>
2020-09-14 11:25:39 -0500

    created ArrestingReport Domain model

---
+++ Police/Police.Domain/Models/ArrestingReportDM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Police.Domain.Models
+{
+    public class ArrestingReportDM
+    {
+        public string ReportID { get; set; }
+        public string ArrestingOfficer { get; set; }
+        public DateTime ArrestDate { get; set; }
+        public int CriminalID { get; set; }
+        public int CrimeID { get; set; }
+    }
+}
