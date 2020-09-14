b914d604ab231528323f6d1589bcad2a159f8caa
Asher Williams <asher.williams1998@gmail.com>
2020-09-14 13:46:46 -0500

    created involved persons domain model

---
+++ Police/Police.Domain/Models/InvolvedPersonsDM.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Police.Domain.Models
+{
+    public class InvolvedPersonsDM
+    {
+        private string _caseNumber;
+
+        public int WitnessID { get; set; }
+        public string CaseNumber 
+        {
+            get => _caseNumber;
+            set
+            {
+                if (value.Length == 0)
+                {
+                    throw new ArgumentException("Case Number cannot be empty", nameof(value));
+                }
+                _caseNumber = value;
+            }
+        }
+
+        public InvolvedPersonsDM(int witness, string caseNumber)
+        {
+            WitnessID = witness;
+            CaseNumber = caseNumber;
+        }
+
+        public InvolvedPersonsDM() { }
+    }
+}
