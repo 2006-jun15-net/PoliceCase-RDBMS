c5e07b405f3d3b5b0efe9d6726ceb11d1ee8c596
Asher Williams <asher.williams1998@gmail.com>
2020-09-15 09:28:02 -0500

    created tests for crime domain model

---
+++ Police/Police.Tests/DomainModels/CrimeTests.cs
@@ -0,0 +1,28 @@
+﻿using Police.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Police.Tests.DomainModels
+{
+    public class CrimeTests
+    {
+        private readonly CrimeDM crime = new CrimeDM();
+
+        [Fact]
+        public void Classification_Valid()
+        {
+            string test = "Test classification";
+            crime.Classification = test;
+
+            Assert.Equal(test, crime.Classification);
+        }
+
+        [Fact]
+        public void ThrowException_When_Classification_Empty()
+        {
+            Assert.ThrowsAny<ArgumentException>(() => crime.Classification = String.Empty);
+        }
+    }
+}
