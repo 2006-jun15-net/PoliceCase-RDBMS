613e17d8aeaedf37628e497342633bd770c6fa99
Asher Williams <asher.williams1998@gmail.com>
2020-09-15 09:42:48 -0500

    create test for involved persons domain model

---
+++ Police/Police.Tests/DomainModels/InvolvedPersonsTests.cs
@@ -0,0 +1,28 @@
+﻿using Police.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Police.Tests.DomainModels
+{
+    public class InvolvedPersonsTests
+    {
+        private readonly InvolvedPersonsDM involvedPersons = new InvolvedPersonsDM();
+
+        [Fact]
+        public void CaseNumber_Valid()
+        {
+            string test = "Test case number";
+            involvedPersons.CaseNumber = test;
+
+            Assert.Equal(test, involvedPersons.CaseNumber);
+        }
+
+        [Fact]
+        public void ThrowException_When_CaseNumber_Empty()
+        {
+            Assert.ThrowsAny<ArgumentException>(() => involvedPersons.CaseNumber = String.Empty);
+        }
+    }
+}
