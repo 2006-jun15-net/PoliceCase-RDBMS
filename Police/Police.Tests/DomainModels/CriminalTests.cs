bb9a25b0c0ad3428cce047d50382324b23cf52f0
Asher Williams <asher.williams1998@gmail.com>
2020-09-15 09:32:46 -0500

    created tests for criminal domain model

---
+++ Police/Police.Tests/DomainModels/CriminalTests.cs
@@ -0,0 +1,58 @@
+﻿using Police.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Police.Tests.DomainModels
+{
+    public class CriminalTests
+    {
+        private readonly CriminalDM criminal = new CriminalDM();
+
+        [Fact]
+        public void FirstName_Valid()
+        {
+            string test = "Test name";
+            criminal.FirstName = test;
+
+            Assert.Equal(test, criminal.FirstName);
+        }
+
+        [Fact]
+        public void ThrowException_When_FirstName_Empty()
+        {
+            Assert.ThrowsAny<ArgumentException>(() => criminal.FirstName = String.Empty);
+        }
+
+        [Fact]
+        public void LastName_Valid()
+        {
+            string test = "Test name";
+            criminal.LastName = test;
+
+            Assert.Equal(test, criminal.LastName);
+        }
+
+        [Fact]
+        public void ThrowException_When_LastName_Empty()
+        {
+            Assert.ThrowsAny<ArgumentException>(() => criminal.LastName = String.Empty);
+        }
+
+        [Fact]
+        public void Sex_Valid()
+        {
+            string test = "Test name";
+            criminal.Sex = test;
+
+            Assert.Equal(test, criminal.Sex);
+        }
+
+        [Fact]
+        public void ThrowException_When_Sex_Empty()
+        {
+            Assert.ThrowsAny<ArgumentException>(() => criminal.Sex = String.Empty);
+        }
+    }
+}
