abed1173b35e221c88ebb1f90863f3c249919533
Asher Williams <asher.williams1998@gmail.com>
2020-09-15 10:11:09 -0500

    created tests for witness domain model

---
+++ Police/Police.Tests/DomainModels/WitnessTests.cs
@@ -0,0 +1,61 @@
+﻿using Police.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Police.Tests.DomainModels
+{
+    public class WitnessTests
+    {
+        private readonly WitnessDM witness = new WitnessDM();
+
+        [Fact]
+        public void FirstName_Valid()
+        {
+            string test = "Test name";
+            witness.FirstName = test;
+
+            Assert.Equal(test, witness.FirstName);
+        }
+
+        [Fact]
+        public void ThrowException_When_FirstName_Empty()
+        {
+            Assert.ThrowsAny<ArgumentException>(() => witness.FirstName = String.Empty);
+        }
+
+        [Fact]
+        public void LastName_Valid()
+        {
+            string test = "Test name";
+            witness.LastName = test;
+
+            Assert.Equal(test, witness.LastName);
+        }
+
+        [Fact]
+        public void ThrowException_When_LastName_Empty()
+        {
+            Assert.ThrowsAny<ArgumentException>(() => witness.LastName = String.Empty);
+        }
+
+        [Theory]
+        [InlineData("Male")]
+        [InlineData("Female")]
+        [InlineData("Other")]
+        public void Sex_Valid(string sex)
+        {
+            witness.Sex = sex;
+            Assert.Equal(sex, witness.Sex);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("test sex")]
+        public void ThrowException_When_Sex_Invalid(string sex)
+        {
+            Assert.ThrowsAny<ArgumentException>(() => witness.Sex = sex);
+        }
+    }
+}
