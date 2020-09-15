3270b79748d3ba2c30f234ccfbc6d543628527b1
Asher Williams <asher.williams1998@gmail.com>
2020-09-15 10:06:32 -0500

    created tests for police report domain model

---
+++ Police/Police.Tests/DomainModels/PoliceReportTests.cs
@@ -0,0 +1,43 @@
+﻿using Police.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Police.Tests.DomainModels
+{
+    public class PoliceReportTests
+    {
+        private readonly PoliceReportDM policeReport = new PoliceReportDM();
+
+        [Fact]
+        public void ID_Valid()
+        {
+            string test = "Test ID";
+            policeReport.ID = test;
+
+            Assert.Equal(test, policeReport.ID);
+        }
+
+        [Fact]
+        public void ThrowException_When_ID_Empty()
+        {
+            Assert.ThrowsAny<ArgumentException>(() => policeReport.ID = String.Empty);
+        }
+
+        [Fact]
+        public void Narrative_Valid()
+        {
+            string test = "Test narrative";
+            policeReport.Narrative = test;
+
+            Assert.Equal(test, policeReport.Narrative);
+        }
+
+        [Fact]
+        public void ThrowException_When_Narrative_Empty()
+        {
+            Assert.ThrowsAny<ArgumentException>(() => policeReport.Narrative = String.Empty);
+        }
+    }
+}
